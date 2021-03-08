9d2c76415692c95876312844018deec2de7af620
Peeke <peekuep@gmail.com>
2021-03-08 17:05:55 +0100

    Add example

--- example/src/App.js
+++ example/src/App.js
@@ -1,5 +1,21 @@
-import { hello }  from '@kaliber/library'
+import { ThemesProvider } from '@kaliber/theming'
+import { themes, ThemeLegacy } from '/components/Theme'
+import styles from './App.css'
 
-export default function App() {
-  return <div>{hello()}</div>
+export default function AppProvider() {
+  return (
+    <ThemesProvider {...{ themes }}>
+      <ThemeLegacy>
+        <App />
+      </ThemeLegacy>
+    </ThemesProvider>
+  )
+}
+
+function App() {
+  return (
+    <div className={styles.component}>
+      Hello
+    </div>
+  )
 }
--- example/src/index.html.js
+++ example/src/index.html.js
@@ -1,5 +1,10 @@
 import App from '/App?universal'
 import javascript from '@kaliber/build/lib/javascript'
+import stylesheet from '@kaliber/build/lib/stylesheet'
+import polyfill from '@kaliber/build/lib/polyfill'
+
+import './index.css'
+import './colorScheme.css'
 
 export default (
   <html lang='en'>
@@ -8,6 +13,8 @@ export default (
       <title>Library</title>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       {javascript}
+      {stylesheet}
+      {polyfill(['default'])}
     </head>
     <body>
       <App />
