5945e45ecbb22d4793efca54c0202ed5a4921a99
Peeke <peekuep@gmail.com>
2021-03-30 17:01:55 +0200

    Initial JSDoc typedef

--- example/src/components/CurrentColor.js
+++ example/src/components/CurrentColor.js
@@ -5,10 +5,14 @@ export function CurrentColor() {
   const theme = useTheme()
 
   return (
-    theme === themes.default ? 'just a color' :
-    theme === themes.brand ? 'a fancy brandcolor' :
-    theme === themes.contrast ? 'a color with contrast' :
-    theme === themes.legacy ? 'an unremarkable legacy color' :
-    'not a color at all...'
+    <>
+      {
+        theme === themes.default ? 'just a color' :
+        theme === themes.brand ? 'a fancy brandcolor' :
+        theme === themes.contrast ? 'a color with contrast' :
+        theme === themes.legacy ? 'an unremarkable legacy color' :
+        'not a color at all...'
+      }
+    </>
   )
 }
--- src/Theme.js
+++ src/Theme.js
@@ -2,10 +2,34 @@ import React from 'react'
 
 const ThemeContext = React.createContext(null)
 
+/**
+ * @typedef {Object<string, string>} Theme
+ * @property {string} backgroundColor
+ * @property {string} color
+ * @property {string} accentColor
+ * @property {string} accentColorContrast
+ * @property {string} [secondaryColor]
+ * @property {string} [secondaryColorContrast]
+ * @property {string} [focusColor]
+ */
+
+/**
+ * @returns {Theme}
+ */
 export function useTheme() {
   return React.useContext(ThemeContext)
 }
 
-export function ThemeProvider({ theme, children }) {
+/**
+ * @param {Object} props
+ * @param {JSX.Element} props.children
+ * @param {Object} props.theme
+ */
+export function ThemeProvider({ children, theme }) {
   return <ThemeContext.Provider value={theme} {...{ children }} />
+}
+
+function NoTypeDefinitionFound() {
+  const theme = useTheme()
+  return null
 }
